26b391861732276cba1ae926becf8a61411ff8d5
shims94 <shims94@gmail.com>
2023-11-13 16:34:09 +0900

    AWS RDS 생성 및 연결 테스트확인, Musical DTO, Repo 추가, Service에 JSON파일 MusicalDTO로 매핑시켜서 DB에 저장하는 메소드 추가

--- web/src/main/java/com/ifive/front/controller/FrontController.java
+++ web/src/main/java/com/ifive/front/controller/FrontController.java
@@ -1,19 +1,18 @@
 package com.ifive.front.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.slf4j.Slf4j;
 
 // 뷰와 연결하는 컨트롤러
+@Slf4j
 @Controller
 public class FrontController {
-    private final Logger log = LoggerFactory.getLogger(FrontController.class);
-    
+
     @RequestMapping(value = "/list/**", method = RequestMethod.GET)
     public String listRequest() {
         return "basic/list";
@@ -31,8 +30,9 @@ public class FrontController {
 
     @GetMapping("/")
     public String drawHome(Model model) {
-        log.info("log message : {} start","home(index.html)");
+        log.info("log message : {} start", "home(index.html)");
 
-       return "index";
+        return "index";
     }
+
 }
