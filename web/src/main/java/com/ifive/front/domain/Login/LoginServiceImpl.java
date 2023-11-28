3d5432496497c7f3a8488e8c1efb65d1f732f5d9
shims94 <shims94@gmail.com>
2023-11-28 09:10:29 +0900

    LoginServiceImple update user->member

--- web/src/main/java/com/ifive/front/domain/Login/LoginServiceImpl.java
+++ web/src/main/java/com/ifive/front/domain/Login/LoginServiceImpl.java
@@ -3,21 +3,21 @@ package com.ifive.front.domain.Login;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class LoginServiceImpl {
-    private final UserRepository userRepository;
+    private final MemberRepository userRepository;
     
     @Autowired
-    public LoginServiceImpl(UserRepository userRepository) {
+    public LoginServiceImpl(MemberRepository userRepository) {
         this.userRepository = userRepository;
     }
 
 
-    public boolean login(User user) {
-        User findUser = userRepository.findByUserId(user.getUserId());
+    public boolean login(Member member) {
+        Member findUser = userRepository.findByMemberId(member.getMemberId());
 
         if(findUser == null) {
             return false;
         }
-        if(!findUser.getPassword().equals(user.getPassword())){
+        if(!findUser.getPassword().equals(member.getPassword())){
             return false;
         }
 
